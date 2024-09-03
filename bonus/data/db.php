390ef8a23517dc36693cbaf34e2b60a81ce93f5a
battistaennio <ennio.battista99@gmail.com>
2024-09-03 17:04:38 +0200

    feat: add more films

--- bonus/data/db.php
+++ bonus/data/db.php
@@ -21,4 +21,24 @@ $movies = [
         ["Domhnall Gleeson", "Oscar Isaac", "Alicia Vikander", "Chelsea Li", "Evie Wray", "Sonoya Mizuno", "Corey Johnson"],
         ["Drammatico", "Fantascienza"]
     ),
+    new Movie(
+        "Inception",
+        "https://mr.comingsoon.it/imgdb/locandine/235x336/47397.jpg",
+        "Christopher Nolan",
+        148,
+        2010,
+        ["Leonardo DiCaprio", "Marion Cotillard", "Elliot Page", "Cillian Murphy", "Michael Caine", "Ken Watanabe", "Joseph Gordon-Levitt", "Tom Hardy", "Tom Berenger", "Lukas Haas", "Tohoru Masamune", "Claire Geare", "Johnathan Geare", "Carl Gilliard", "Daniel Girondeaud", "Mobin Khan", "Yuji Okumoto", "Nicole Pulliam", "Dileep Rao", "Talulah Riley"],
+        ["Fantascienza", "Thriller", "Azione"]
+    ),
+    new Movie(
+        "Avatar",
+        "https://mr.comingsoon.it/imgdb/locandine/235x336/846.jpg",
+        "James Cameron",
+        166,
+        2009,
+        ["Sam Worthington", "Sigourney Weaver", "Giovanni Ribisi", "Michelle Rodriguez", "Zoe Saldana", "Joel David Moore", "Laz Alonzo", "Wes Studi", "Stephen Lang", "Peter Mensah", "CCH Pounder", "Dileep Rao", "Matt Gerald", "Scott Lawrence", "Jacob Tomuri"],
+        ["Fantascienza", "Thriller", "Avventura", "Azione"]
+    ),
+
+
 ];
